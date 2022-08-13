996671c4eeaa39fb072f99696d1fd5caa326b6ff
minh-vu-kma <59890736+minh-vu-kma@users.noreply.github.com>
2022-08-13 18:45:37 +0700

    Create upload_docker.sh

---
+++ upload_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath="vuthaiminh98/minhvt9:v1.0.0"
+
+# Step 2:  
+# Authenticate & tag
+docker login
+docker tag minhvt9 $dockerpath
+echo "Docker ID and Image: $dockerpath"
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
